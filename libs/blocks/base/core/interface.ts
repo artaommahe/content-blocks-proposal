45540e129579d4656ce4f2e738a6b6a6175923b4
Popov Max <drowpmv@gmail.com>
2019-02-17 18:18:51 +0300

    updated proporsal doc

--- libs/blocks/base/core/interface.ts
+++ libs/blocks/base/core/interface.ts
@@ -1,9 +1,5 @@
 import { IBlockScoreConfig } from '../score/interface';
 import { IBlockSyncConfig } from '../sync/interface';
-import { BlockBaseModel } from '../model/base';
-import { BlockConfig } from '../config/config';
-import { BlockBaseScoreStrategy } from '../score/strategy/base';
-import { BlockBaseSyncStrategy } from '../sync/strategy/base';
 
 export type TBlockId = string;
 
@@ -11,15 +7,4 @@ export interface IBlockConfig {
   score?: IBlockScoreConfig;
   sync?: IBlockSyncConfig;
   isMobile?: boolean;
-}
-
-export interface IBlockApiConfig<
-  TModel extends BlockBaseModel<any, any>,
-  TScoreStrategy extends BlockBaseScoreStrategy<TModel, any, any>,
-> {
-  blockId?: TBlockId;
-  blockConfig?: BlockConfig;
-  model?: TModel;
-  scoreStrategy?: Constructor<TScoreStrategy>;
-  syncStrategy?: typeof BlockBaseSyncStrategy;
 }
